4e0c43d89f54c071ce8a2fb6f028c66c92bf1a43
reneesanders <micromunkey@gmail.com>
2016-09-10 23:28:50 -0700

    added questions for the flowchart to determine what products are best

--- index.php
+++ index.php
@@ -15,6 +15,30 @@ echo "
 		</ul>
 	</div>
 
+	<div id='do you like to do your nails?' class ='container'>
+	</div>
+
+	<div id='do you mind dry time of nail polish?' class='container'
+	</div>
+
+	<div id='do you like changing your nail color to match your outfit?' class='container'
+	</div>
+
+	<div id='do you like fun nail art?' class='container'
+	</div>
+
+	<div>'do you have a hard time painting your nails?' class='container'
+	</div>
+
+	<div id='do you like getting gels at the salon?' class='container'
+	</div>
+
+	<div id='are you willing to try something new?' class='container'
+	</div>
+
+	<div id='name' class='container'>
+	</div>
+
 	<div class='container'>
 		<h3><a href='https://reneesanders.jamberry.com'> Shop Now</a></h3>
 		<a href='mailto:micromunkey@gmail.com'> <h3> Questions? Email Me! </h3> </a>
