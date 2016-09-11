ee280e0232f1c02d8cac80e946ad07e434844446
reneesanders <micromunkey@gmail.com>
2016-09-10 21:34:09 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "
+<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+<link rel='stylesheet' type='text/css' href='css/styles.css'>
+<title>Ye Olde Jamberry Survey </title>
+	<div class='container'>
+		<h1><b><i>Hello Ladies!</b></i></h1>
+		<h2>This is going to be a Jamberry Master Survey!</h2>
+		<p> I'm learning PHP and html stuffs so that I can do a survey that encompasses the following topics:</p>
+		<ul>
+			<li>What Jamberry products are best for you?</li>
+			<li>Personail Style assessment</li>
+			<li>Sample Request</li>
+		</ul>
+	</div>
+
+	<div class='container'>
+		<h3><a href='https://reneesanders.jamberry.com'> Shop Now</a></h3>
+		<a href='mailto:micromunkey@gmail.com'> <h3> Questions? Email Me! </h3> </a>
+	</div>"
+?>
