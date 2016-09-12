0aac237f351cfd65650c754d19784daa4d2a0de8
reneesanders <micromunkey@gmail.com>
2016-09-12 13:12:14 -0700

    several changes to user input and css

--- index.php
+++ index.php
@@ -15,30 +15,60 @@ echo "
 		</ul>
 	</div>
 
-	<div id='do you like to do your nails?' class ='container'>
+	<div id='jamberryproducts' class='container'>
+		<h1>What Jamberry products are best for you? </h1> 
 	</div>
 
-	<div id='do you mind dry time of nail polish?' class='container'
+	<div id='question-one' class ='container'>
+		<h1>Do you like to do your nails? </h1>
 	</div>
 
-	<div id='do you like changing your nail color to match your outfit?' class='container'
+	<div id='question-two' class='container'>
 	</div>
 
-	<div id='do you like fun nail art?' class='container'
+	<div id='question-three' class='container'>
 	</div>
 
-	<div>'do you have a hard time painting your nails?' class='container'
+	<div id='question-four' class='container'>
 	</div>
 
-	<div id='do you like getting gels at the salon?' class='container'
+	<div id='question-five' class='container'>
 	</div>
 
-	<div id='are you willing to try something new?' class='container'
+	<div id='question-six' class='container'>
 	</div>
 
-	<div id='name' class='container'>
+	<div id='question-seven' class='container'>
 	</div>
 
+	<div id='Personail Style' class='container'>
+	<h1>Personail Syle</h1> 
+	</div>
+
+	<div id='user info' class='container'>
+	<h1> <b>Personal Info </b></h2>
+		<div>
+		<label for='name'>Name:</label>
+		<input name='name' type='text' maxlength='50'/>
+
+		<label for='birthday'>Birthday:</label>
+		<input name='birthday' type='text'>
+
+		<label for='favoriteholiday'>Favorite Holiday:</label>
+		<input name='favoriteholiday' type='text'> 
+		</div>
+		
+		<div class='col-sm-6 container'>
+			<textarea name='description'cols='20' rows='4'>Describe yourself...
+			</textarea>
+		</div>
+
+		<div class='col-sm-6 container'>
+			<textarea name='hobbies' cols='20' rows='4'> List your hobbies...
+			</textarea>
+		</div>
+	</div>
+	
 	<div class='container'>
 		<h3><a href='https://reneesanders.jamberry.com'> Shop Now</a></h3>
 		<a href='mailto:micromunkey@gmail.com'> <h3> Questions? Email Me! </h3> </a>
---
+++ textquestionsforflowchart.php
@@ -0,0 +1,24 @@
+
+
+
+//Questions for Flowchart
+
+'do you like to do your nails?' 
+	
+
+'do you mind dry time of nail polish?' 
+	
+
+'do you like changing your nail color to match your outfit?'
+
+'do you like fun nail art?' 
+
+'do you have a hard time painting your nails?' 
+
+'do you like getting gels at the salon?' 
+
+'are you willing to try something new?' 
+
+Dark Purple: 732C65
+Light Purple: D982BA
+Pink Fizz: C62A86
